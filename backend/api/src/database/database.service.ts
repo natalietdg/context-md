fc56305cd0d49fb87e643b88802958a329d0ca7c
Natalie Leong <natalie.leong@paywatchglobal.com>
2025-09-16 02:50:30 +0800

    refactor: parse port as int

--- backend/api/src/database/database.service.ts
+++ backend/api/src/database/database.service.ts
@@ -77,7 +77,7 @@ export class DatabaseService {
   async getDatabaseConfig() {
     return {
       host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
+      port: parseInt(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
