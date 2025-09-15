f5845d7fb434d6f51dc7913f9cafe99109871703
Natalie Leong <natalie.leong@paywatchglobal.com>
2025-09-16 04:12:25 +0800

    refactor: add ca

--- backend/api/src/database/database.module.ts
+++ backend/api/src/database/database.module.ts
@@ -3,6 +3,8 @@ import { DatabaseService } from './database.service';
 import { DatabaseController } from './database.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as entities from '../entities';
+import * as fs from 'fs';
+import * as path from 'path';
 
 // database.module.ts
 @Module({
@@ -15,6 +17,7 @@ import * as entities from '../entities';
             password: process.env.DATABASE_PASSWORD ?? '',
             database: process.env.DATABASE_NAME ?? 'contextmd',
             ssl: process.env.ENVIRONMENT === "production" ? {
+                ca: fs.readFileSync(path.join(__dirname, '../rds-ca-2019-root.pem')).toString(),
                 rejectUnauthorized: false,
             } : false,
             entities: [
